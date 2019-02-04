1d5717dd5c020d8d35c4bb3c05d8aaf5b7bd486d
hanhouqi <hanhouqi@sxqinkuai.com>
2019-02-04 09:28:11 +0800

    删除冗余代码

--- src/main/java/com/hqhan/Application.java
+++ src/main/java/com/hqhan/Application.java
@@ -1,22 +1,16 @@
 package com.hqhan;
 
 
-import java.io.OutputStream;
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 /**
- * @title: Application
- * @description:
- * @email: hqhan8080@Gmail.com
- * @auther: hanhouqi
- * @date: 2018/9/8 02:03
+ * description
+ *
+ * @author hqhan8080@Gmail.com
+ * @version v1.0
+ * @date 2018/9/8 02:03
  */
 
 @SpringBootApplication
